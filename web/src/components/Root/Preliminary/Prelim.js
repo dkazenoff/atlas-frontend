e4b037faa78f5c8d5621ad39f9d1aeda08f1876d
Dan K <dkazenoff@gmail.com>
2020-06-18 13:59:48 -0400

    Edits to styles

--- web/src/components/Root/Preliminary/Prelim.js
+++ web/src/components/Root/Preliminary/Prelim.js
@@ -1,34 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Button, Container } from 'react-bootstrap';
 import NavComponent from "./../../Interface/Navbar";
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
 export default function Prelim() {
+    const [credscore, setCredscore] = useState(0);
+
     return (
         <div d-flex mb-2 justify-content-between>
             <NavComponent />
-            <Jumbotron fluid>
-                <Container>
-                    <h1>Making Collateral-Free Crypto Loans Reality</h1>
+            <Jumbotron fluid bg-primary text-white>
+                <Container >
+                    {/* <div className="container" bg-primary text-white> */}
+                    <h1>Making Collateral-Free Crypto Loans a Reality</h1>
                     <p>
-                        Please input below your known credentials in order to be presented loan categories best suited for you.
-                 </p>
+                        Please input below your Pre-Check credentials in order to be presented loan categories best suited for you.
+                        </p>
+                    {/* </div> */}
                 </Container>
             </Jumbotron>
             <Form >
                 <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Label>What is your Credit Score?</Form.Label>
+                    <Form.Control type="email" placeholder="Enter Credit Score" />
                     <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                </Form.Text>
+                    </Form.Text>
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Label>Approximate Annual Salary:</Form.Label>
+                    <Form.Control type="password" placeholder="$1000000" />
                 </Form.Group>
                 <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
+                    <Form.Check type="checkbox" label="I'm Not a Robot" />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
