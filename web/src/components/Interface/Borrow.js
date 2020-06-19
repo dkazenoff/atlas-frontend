4b72a5fd8e565f39032ec52014ff9c3aeebcc838
Dan K <dkazenoff@gmail.com>
2020-06-18 23:08:57 -0400

    Date/time install. Fixing up routes

--- web/src/components/Interface/Borrow.js
+++ web/src/components/Interface/Borrow.js
@@ -1,9 +1,9 @@
-import React from 'react'
+// import React from 'react'
 
-export default function Borrow() {
-    return (
-        <div>
-            hello
-        </div>
-    )
-}
+// export default function Borrow() {
+//     return (
+//         <div>
+//             hello
+//         </div>
+//     )
+// }
