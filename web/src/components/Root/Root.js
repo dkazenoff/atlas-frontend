f9ece8bbac582721d39e5ac2b43749fb96919bbe
Dan K <dkazenoff@gmail.com>
2020-06-25 04:58:24 -0400

    Commenting out web3 glitches on Router; Likely incorrect installation of web3-react was rectified

--- web/src/components/Root/Root.js
+++ web/src/components/Root/Root.js
@@ -6,22 +6,32 @@ import Home from "./Home";
 import ContractEngine from "./Home/ContractEngine";
 import Borrower from "./BorrowCreds";
 
+// import { Web3Provider, Connectors } from 'web3-react';
+
+
 
 const Root = () => {
+    // const { InjectedConnector } = Connectors
+    // const MetaMask = new InjectedConnector({ supportedNetworks: [1, 4] })
+    // const connectors = { MetaMask }
     return (
-        <div>
-            <div>
-                {/* <Link to="/home">Home</Link>
-                <Link to="/">Pre-Check</Link> */}
-            </div>
-            <Switch>
-                <Route component={ContractEngine} path="/home" />
-                <Route exact component={Prelim} path="/" />
-                <Route component={Borrower} path="/borrow" />
-                {/* <Route component={Borrower} path="/b-home" /> */}
 
-            </Switch>
-        </div>
+
+
+        <Switch>
+            {/* <Web3Provider
+                connectors={MetaMask}
+                libraryName={'web3'} > */}
+
+            <Route component={ContractEngine} path="/home" />
+            <Route exact component={Prelim} path="/" />
+            <Route component={Borrower} path="/borrow" />
+            {/* <Route component={Borrower} path="/b-home" /> */}
+            {/* </Web3Provider> */}
+
+        </Switch>
+
+
     )
 };
 
